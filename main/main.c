a7d67a98a8d55ac739926169bd921421f2529b4f
w-mj <wmj@alphamj.cn>
2022-04-08 20:04:36 +0800

    init

---
+++ main/main.c
@@ -0,0 +1,297 @@
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "lwip/sockets.h"
+#include <lwip/netdb.h>
+
+#include "esp_netif.h"
+
+
+#define EXAMPLE_ESP_MAXIMUM_RETRY  3
+
+struct {
+    char sta_ssid[32];
+    char sta_password[32];
+} config;
+
+
+static const char *TAG = "light";
+
+static int s_retry_num = 0;
+
+static void tcp_server_task();
+void startAp();
+
+void startSta();
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        // 初始化WIFI成功
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            // WIFI连接失败，尝试重连
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            // 重试三次失败，启动AP模式
+            s_retry_num = 0;
+            startAp();
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        // 连接WIFI成功
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        // xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+        // Start TCP server.
+        // xTaskCreate(tcp_server_task, "tcp_server", 4096, NULL, 5, NULL);
+    } else if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        // AP模式有客户端连接
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        // AP模式客户端断开连接
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+}
+
+void wifi_init() {
+    
+}
+
+void startSta() {
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, NULL));
+
+    wifi_config_t cf1;
+    strcpy((char*)cf1.sta.ssid, config.sta_ssid);
+    strcpy((char*)cf1.sta.password, config.sta_password);
+    cf1.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    cf1.sta.pmf_cfg.capable = true;
+    cf1.sta.pmf_cfg.required = false;
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &cf1) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+}
+
+void hexToChar2(uint8_t hex, char *str) {
+    unsigned char val = hex >> 4;
+    str[0] = val + (val < 10? '0' : ('A' - 10));
+    val = hex & 0xF;
+    str[1] = val + (val < 10? '0' : ('A' - 10));
+}
+
+void startAp() {
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_t* wifiAP = esp_netif_create_default_wifi_ap();
+
+    esp_netif_ip_info_t ipInfo;
+    IP4_ADDR(&ipInfo.ip, 192,168,222,1);
+    IP4_ADDR(&ipInfo.gw, 0,0,0,0);
+    IP4_ADDR(&ipInfo.netmask, 255,255,255,0);
+    esp_netif_dhcps_stop(wifiAP);
+    esp_netif_set_ip_info(wifiAP, &ipInfo);
+    esp_netif_dhcps_start(wifiAP);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL));
+
+    uint8_t mac[10];
+    char myMacStr[18] = {0};
+    char buf[32] = {0};
+    ESP_ERROR_CHECK(esp_read_mac(mac, ESP_MAC_WIFI_SOFTAP));
+    for (int i = 0; i < 8; i++) {
+        hexToChar2(mac[i], myMacStr + 2 * i);
+    }
+    strcpy(buf, "RGBLIGHT-");
+    strcat(buf, myMacStr);
+
+    wifi_config_t cf1 = {
+        .ap = {
+            .channel = 4,
+            .password = "12345678",
+            .max_connection = 2,
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK
+        },
+    };
+    strcpy((char*)cf1.ap.ssid, buf);
+    cf1.ap.ssid_len = strlen(buf);
+
+    ESP_LOGI(TAG, "Start AP SSID:%s, PASSWORD:%s", buf, "12345678");
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &cf1));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+static void do_retransmit(const int sock)
+{
+    int len;
+    char rx_buffer[128];
+
+    do {
+        len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+        if (len < 0) {
+            ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
+        } else if (len == 0) {
+            ESP_LOGW(TAG, "Connection closed");
+        } else {
+            rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
+            ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
+
+            // send() can return less bytes than supplied length.
+            // Walk-around for robust implementation.
+            int to_write = len;
+            while (to_write > 0) {
+                int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
+                if (written < 0) {
+                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                }
+                to_write -= written;
+            }
+
+            len = snprintf(rx_buffer, 128, "Free Heap size: %d Bytes\n", esp_get_free_heap_size());
+            to_write = len;
+            while (to_write > 0) {
+                int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
+                if (written < 0) {
+                    ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                }
+                to_write -= written;
+            }
+        }
+    } while (len > 0);
+}
+
+static void tcp_server_task()
+{
+    char addr_str[128];
+    int addr_family = AF_INET;
+    struct sockaddr_storage dest_addr;
+
+    struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
+    dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
+    dest_addr_ip4->sin_family = AF_INET;
+    dest_addr_ip4->sin_port = htons(4617);
+
+    int listen_sock = socket(addr_family, SOCK_STREAM, IPPROTO_IP);
+    if (listen_sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        vTaskDelete(NULL);
+        return;
+    }
+    int opt = 1;
+    setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    ESP_LOGI(TAG, "Socket created");
+
+    int err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    if (err != 0) {
+        ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        ESP_LOGE(TAG, "IPPROTO: %d", addr_family);
+        goto CLEAN_UP;
+    }
+    ESP_LOGI(TAG, "Socket bound, port %d", 4617);
+
+    err = listen(listen_sock, 1);
+    if (err != 0) {
+        ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
+        goto CLEAN_UP;
+    }
+
+    while (1) {
+        ESP_LOGI(TAG, "Socket listening");
+        struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
+        socklen_t addr_len = sizeof(source_addr);
+        int sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+            break;
+        }
+
+        // Convert ip address to string
+        
+        inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
+
+        ESP_LOGI(TAG, "Socket accepted ip address: %s", addr_str);
+
+        do_retransmit(sock);
+
+        shutdown(sock, 0);
+        close(sock);
+    }
+
+CLEAN_UP:
+    close(listen_sock);
+    vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    nvs_handle_t nvs_handle;
+    ret = nvs_open("storage", NVS_READWRITE, &nvs_handle);
+    if (ret != ESP_OK) {
+        printf("Error (%s) opening NVS handle!\n", esp_err_to_name(ret));
+    }
+
+    config.sta_ssid[0] = 0;
+    size_t length = 0;
+    ret = nvs_get_str(nvs_handle, "sta_ssid", config.sta_ssid, &length);
+    if (ret == ESP_OK) {
+        ret = nvs_get_str(nvs_handle, "sta_password", config.sta_password, &length);
+        if (ret != ESP_OK) {
+            config.sta_ssid[0] = 0;
+        }
+    }
+
+    wifi_init();
+
+    if (config.sta_ssid[0] == 0) {
+        ESP_LOGI(TAG, "Cannot get wifi info, run as ap mode");
+        startAp();
+    } else {
+        ESP_LOGI(TAG, "Get wifi info ssid:%s, password:%s", config.sta_ssid, config.sta_password);
+        startSta();
+    }
+
+    ESP_LOGI(TAG, "START Socket");
+    xTaskCreate(tcp_server_task, "tcp_server", 4096, NULL, 5, NULL);
+
+}
